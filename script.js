0b7581018d037a344eec335c90e66a22b60e7c09
Dominik <54892176+Ghardir@users.noreply.github.com>
2022-01-22 16:44:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+
+
+// a wiec tak...lancuch produkcji wyglada tak:
+
+ // jedna kopalnia
+ // jeden do wegla drzewnego
+ 
+ // =>> one oba produkuja odpowiednio iron_ore i charcoal w ilosci 2/min i one leca do
+ // =>> przzetworzenia na iron w hucie, ktora wytwarza 2iron z 2ironore i 2 charcoal (czyli stosunki 1:1:1)
+ 
+ // i to oto zelazo leci do warsztatu
+ // ktory zuzywa 0.5 iron by zrobic 2 tools, czyli lancuch wyglada tak:
+ 
+ //no i w pisdu... charcoal burner potrzebuje drewna przeciez.... czyli musimy miec 
+					//drwala i jego chatke....
+					//lumberjack lumber- 1.5/min
+					
+					//charcoal hut potrzebuje 2/min lumber... czyli.....
+					
+					//4x lumberjack -> 3 x charcoal burner
+ 
+ 
+// -------------already edited for wood
+
+// tier I
+	// 1x iron mine -> 2 ore
+	// 4x lumberjacks hut -> 6 wood
+	
+// tier II
+			//    	6 wood -> 3x charcoal burner -> 6 coal
+			// 	 		6 coal -> 6 iron -> 3x iron smelter -> 6 iron
+// tier III
+					//	 	0.5 -> 12x workshop -> 24 tools
+					
+					
+					
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
