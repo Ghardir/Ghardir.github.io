32af1527d35c2d9f8f8eec0794c878b4558a16d8
Dominik <Ghardirz2@gmail.com>
2022-01-26 10:34:28 +0100

    upgrade

--- script.js
+++ script.js
@@ -1,44 +1,143 @@
 
+function updateResources(){
+	produceIronore();
+	produceLumber();
+};
 
-// a wiec tak...lancuch produkcji wyglada tak:
 
- // jedna kopalnia
- // jeden do wegla drzewnego
- 
- // =>> one oba produkuja odpowiednio iron_ore i charcoal w ilosci 2/min i one leca do
- // =>> przzetworzenia na iron w hucie, ktora wytwarza 2iron z 2ironore i 2 charcoal (czyli stosunki 1:1:1)
- 
- // i to oto zelazo leci do warsztatu
- // ktory zuzywa 0.5 iron by zrobic 2 tools, czyli lancuch wyglada tak:
- 
- //no i w pisdu... charcoal burner potrzebuje drewna przeciez.... czyli musimy miec 
-					//drwala i jego chatke....
-					//lumberjack lumber- 1.5/min
-					
-					//charcoal hut potrzebuje 2/min lumber... czyli.....
-					
-					//4x lumberjack -> 3 x charcoal burner
- 
- 
-// -------------already edited for wood
+class ProductionBuilding {
+	constructor(name, production, productionRate, amount, workforce, maxWorkforce, inCoins, inTools, upkeep){
+		this.name = name;
+		this.production = production;
+		this.productionRate = productionRate; //in one minute/ one cycle at 100% efficiency
+		this.amount = amount;
 
-// tier I
-	// 1x iron mine -> 2 ore
-	// 4x lumberjacks hut -> 6 wood
-	
-// tier II
-			//    	6 wood -> 3x charcoal burner -> 6 coal
-			// 	 		6 coal -> 6 iron -> 3x iron smelter -> 6 iron
-// tier III
-					//	 	0.5 -> 12x workshop -> 24 tools
-					
-					
-					
+		//workers control
+		this.workforce = workforce;
+		this.maxWorkforce = maxWorkforce;
+		//cost
+		this.inCoins = inCoins;
+		this.inTools = inTools;
+		this.upkeep = upkeep;
+	}	
+};
 
- 
- 
- 
- 
- 
- 
- 
+// all production buildings
+
+const lumberjack = new ProductionBuilding ("Lumberjack", "Lumber", 1.5, 0, 0, 0, 50, 2, 0);
+const ironMine = new ProductionBuilding ("Iron mine", "Ironore", 2, 0, 0, 0, 200, 10, 0);
+
+// materials and starting conditions
+let ironore = 5;
+let lumber = 0;
+let charcoal= 0;
+let iron = 1000;
+let tools = 2000;
+let coins = 1500;
+let workers = 100; //not implemented xd
+
+document.querySelector("#ore-counter").innerHTML = ironore
+document.querySelector("#lumber-counter").innerHTML = lumber;
+document.querySelector("#charcoal-counter").innerHTML = charcoal;
+document.querySelector("#iron-counter").innerHTML = iron;
+document.querySelector("#tools-counter").innerHTML = tools;
+
+//all buttons
+
+document.querySelector("#give-worker-iron-mine").addEventListener("click", addWorker);
+document.querySelector("#take-worker-iron-mine").addEventListener("click", takeWorker);
+document.querySelector("#buy-ironmine").addEventListener("click", buyIronMine);
+
+document.querySelector("#give-worker-lumberjack").addEventListener("click", addWorker2);
+document.querySelector("#take-worker-lumberjack").addEventListener("click", takeWorker2);
+document.querySelector("#buy-lumberjack").addEventListener("click", buyLumberjack);
+
+
+//ironmine functions
+
+function buyIronMine (){
+	if (coins >= ironMine.inCoins) {
+	ironMine.amount += 1;
+	coins -= ironMine.inCoins;
+	document.querySelector("#iron-mine-counter").innerHTML = ironMine.amount;
+	document.querySelector("#gold-coins-counter").innerHTML = coins;
+	ironMine.maxWorkforce = 2*ironMine.amount;
+	document.querySelector("#max-workers-iron-mine").innerHTML = ironMine.maxWorkforce;
+	}else{
+
+	}
+};
+
+function addWorker(){
+	if (ironMine.workforce < ironMine.maxWorkforce){
+		ironMine.workforce += 1;
+		document.querySelector("#iron-mine-counter-worker").innerHTML = ironMine.workforce;
+	}else{
+
+	}
+};
+
+
+function takeWorker() {
+	if (ironMine.workforce == 0) {
+	}else{
+		ironMine.workforce -= 1;
+		document.querySelector("#iron-mine-counter-worker").innerHTML = ironMine.workforce;
+	}
+};
+
+function produceIronore() {
+	ironore += ironMine.productionRate*ironMine.workforce/2;
+	let ironoreStr = ironore.toString().split(".")
+	document.querySelector("#ore-counter").innerHTML = ironoreStr[0]
+}
+
+
+
+//lumberjacks functions
+
+
+
+function buyLumberjack (){
+	if (coins >= lumberjack.inCoins) {
+	lumberjack.amount += 1;
+	coins -= lumberjack.inCoins;
+	document.querySelector("#lumberjack-counter").innerHTML = lumberjack.amount;
+	document.querySelector("#gold-coins-counter").innerHTML = coins;
+	lumberjack.maxWorkforce = 2*lumberjack.amount;
+	document.querySelector("#max-workers-lumberjack").innerHTML = lumberjack.maxWorkforce;
+	}else{
+
+	}
+};
+
+function addWorker2(){
+	if (lumberjack.workforce < lumberjack.maxWorkforce){
+		lumberjack.workforce += 1;
+		document.querySelector("#lumberjack-counter-worker").innerHTML = lumberjack.workforce;
+	}else{
+
+	}
+};
+
+
+function takeWorker2() {
+	if (lumberjack.workforce == 0) {
+	}else{
+		lumberjack.workforce -= 1;
+		document.querySelector("#lumberjack-counter-worker").innerHTML = lumberjack.workforce;
+	}
+};
+
+function produceLumber() {
+	lumber += lumberjack.productionRate*lumberjack.workforce/2;
+	let lumberStr = lumber.toString().split(".")
+	document.querySelector("#lumber-counter").innerHTML = lumberStr[0]
+}
+
+
+
+
+
+
+
